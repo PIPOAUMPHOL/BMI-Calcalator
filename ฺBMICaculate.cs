15ef6747b37abb77063ae581466d8c4f38966c60
PIPOAUMPHOL <aumpholllo@gmail.com>
2024-01-25 06:49:46 +0700

    feat: my first commit

---
+++ ฺBMICaculate.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace learnCSharp {
+    class Program {
+        static void Main(string[] args) {
+			double x = 100.15;
+int y = (int)x;
+
+Console.Write(y);
+        }
+    }
+};
